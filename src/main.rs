be4e3134e63b94f8d6c82f174b80b993f7874b6c
hzb <2324307175@qq.com>
2022-03-04 12:58:47 +0800

    Write success

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nix::pty::{openpty};
 use nix::unistd::read;
+use std::fmt::format;
 use std::os::unix::io::RawFd;
 use std::process::{Command, Child};
 use std::vec;
@@ -9,6 +10,13 @@ use std::os::unix::io::FromRawFd;
 use std::process::Stdio;
 
 // Vec<u8>: UTF8 encoded sequences.
+
+// The Process for pty:
+// Set process stdin & stdout & stderr as the slave (shell).
+// The terminal emulator have the master file.
+// We write content to the master file. The master file will send it to the slave file as input for the process.
+// The shell process will execute the command and return the output to slave file.
+// The slave file will then have it's content pushed to the master file. We read from it for display.s
 
 struct bipty {
     process: Child,
@@ -43,16 +51,11 @@ unsafe fn spawn_pty_with_shell(default_shell: String) -> bipty {
             println!("master fd: {}, slave fd: {}", &master, &slave);
             // If the result is a child process, spawn a new shell.
             let builder = Command::new(&default_shell)
-            // Get input from the slave file descriptor.
             .stdin(Stdio::from_raw_fd(slave))
-            // .stdout(Stdio::from_raw_fd(slave))
-            // .stderr(Stdio::from_raw_fd(slave))
+            .stdout(Stdio::from_raw_fd(slave))
+            .stderr(Stdio::from_raw_fd(slave))
             .spawn()
             .expect("failed to spawn");
-
-            // println!("{:?}", builder.stdin);
-            // println!("{:?}", builder.stdout);
-            // println!("{:?}", builder.stderr);
 
             // wait for 2s and then exit.
             std::thread::sleep(std::time::Duration::from_millis(2000));
@@ -72,21 +75,21 @@ unsafe fn spawn_pty_with_shell(default_shell: String) -> bipty {
 // Execute a command with user input, by flushing master file descripter.
 fn pty_execute(mfd: RawFd, command: &str) {
     let mut master_file = unsafe { File::from_raw_fd(mfd) };
-    // Change the file buffer.
+    // Write command to the file descriptor.
     write!(master_file, "{}", command).unwrap();
-    // Write it out.
     master_file.flush().unwrap();
 }
 
-// fn read_from_master(mfd: RawFd) {
+// Error: Bad file descriptor code 9.
+// fn read_from_master(mfd: RawFd) -> String {
 //     let mut master_file = unsafe { File::from_raw_fd(mfd) };
 
 //     let mut read_buffer = String::new();
 //     master_file.read_to_string(&mut read_buffer).unwrap(); // Execution stops here.
-//     println!("master file descriptor content: {}", read_buffer);
+//     format!("Master file content: {}", read_buffer)
 // }
 
-fn read_from_master_fd(mfd: RawFd) {
+fn read_from_master_fd(mfd: RawFd) -> String {
     let mut read_buffer: Vec<u8> = vec![];
 
     loop {
@@ -95,13 +98,15 @@ fn read_from_master_fd(mfd: RawFd) {
                 read_buffer.append(&mut read_bytes);
             }
             None => {
-                println!("{:?}", String::from_utf8(read_buffer).unwrap());
                 break;
             }
         }
     }
+
+    format!("{:?}", String::from_utf8(read_buffer).unwrap())
 }
 
+//TODO: Read from the master file without pending the terminal or panic with Bad file desciptor code 9.
 fn main() {
     let command1 = "touch /home/hzb/Desktop/itworks\n";
     let command2 = "echo hzb\n";
@@ -119,12 +124,13 @@ fn main() {
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
+        let content: String = read_from_master_fd(bidirect_pty.mfd);
+
         pty_execute(bidirect_pty.mfd, command2);
         
-        // read_from_master(&mut master_file);
-        read_from_master_fd(bidirect_pty.mfd);
+        println!("{}", content);
 
-        pty_execute(bidirect_pty.mfd, command1);
+        // pty_execute(bidirect_pty.mfd, command1);
 
         std::process::exit(0);
     }
