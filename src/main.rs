8cc8b056246f695297064e1818e073e36317ed01
hzb <2324307175@qq.com>
2022-03-09 17:09:52 +0800

    clean main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use
+use std::rc::Rc;
 
+use rustcmd::mux::Mux;
+use rustcmd::pty::PtySize;
 fn main() {
+    let mux = Rc::new(Mux::new(PtySize::default()).unwrap());
+    Mux::set_mux(&mux);
 
+    mux.start().unwrap();
 }
