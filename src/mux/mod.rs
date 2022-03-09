7bf17b645604f097abcf1a0821811957069f3921
hzb <2324307175@qq.com>
2022-03-09 17:05:04 +0800

    remove gui

--- src/mux/mod.rs
+++ src/mux/mod.rs
@@ -1,32 +1,106 @@
-use crate::config::Config;
-use crate::core::hyperlink::Hyperlink;
-use crate::core::promise;
-use crate::core::ratelim::RateLimiter;
 use crate::mux::tab::Tab;
 use crate::pty::{unix, PtySize, PtySystem};
-use crate::term::clipboard::Clipboard;
-use crate::term::TerminalHost;
-use anyhow::bail;
 use std::cell::{Ref, RefCell};
 use std::io::Read;
 use std::process::Command;
 use std::rc::Rc;
-use std::sync::Arc;
 use std::thread;
+use ratelimit_meter::algorithms::NonConformance;
+use ratelimit_meter::{DirectRateLimiter, LeakyBucket, NegativeMultiDecision};
 
 pub mod tab;
 
 pub struct Mux {
     tab: RefCell<Tab>,
-    config: Arc<Config>,
 }
 
-fn read_from_tab_pty(config: Arc<Config>, mut reader: Box<dyn std::io::Read>) {
+pub struct RateLimiter {
+    lim: DirectRateLimiter<LeakyBucket>,
+}
+
+impl RateLimiter {
+    pub fn new(capacity_per_second: u32) -> Self {
+        Self {
+            lim: DirectRateLimiter::<LeakyBucket>::per_second(
+                std::num::NonZeroU32::new(capacity_per_second)
+                    .expect("RateLimiter capacity to be non-zero"),
+            ),
+        }
+    }
+
+    pub fn blocking_admittance_check(&mut self, amount: u32) {
+        loop {
+            match self.lim.check_n(amount) {
+                Ok(_) => return,
+                Err(NegativeMultiDecision::BatchNonConforming(_, over)) => {
+                    let duration = over.wait_time_from(std::time::Instant::now());
+                    std::thread::sleep(duration);
+                }
+                Err(err) => panic!("{}", err),
+            }
+        }
+    }
+}
+
+use async_task::{JoinHandle, Task};
+use std::future::Future;
+use std::sync::Mutex;
+
+pub type SpawnFunc = Box<dyn FnOnce() + Send>;
+pub type ScheduleFunc = Box<dyn Fn(Task<()>) + Send + Sync + 'static>;
+
+fn no_schedule_configured(_: Task<()>) {
+    panic!("no scheduler has been configured");
+}
+
+lazy_static::lazy_static! {
+    static ref ON_MAIN_THREAD: Mutex<ScheduleFunc> = Mutex::new(Box::new(no_schedule_configured));
+    static ref ON_MAIN_THREAD_LOW_PRI: Mutex<ScheduleFunc> = Mutex::new(Box::new(no_schedule_configured));
+}
+
+pub fn set_schedulers(main: ScheduleFunc, low_pri: ScheduleFunc) {
+    *ON_MAIN_THREAD.lock().unwrap() = Box::new(main);
+    *ON_MAIN_THREAD_LOW_PRI.lock().unwrap() = Box::new(low_pri);
+}
+
+pub fn spawn<F, R>(future: F) -> JoinHandle<R, ()>
+where
+    F: Future<Output = R> + 'static,
+    R: 'static,
+{
+    let (task, handle) =
+        async_task::spawn_local(future, |task| ON_MAIN_THREAD.lock().unwrap()(task), ());
+    task.schedule();
+    handle
+}
+
+pub fn spawn_into_main_thread<F, R>(future: F) -> JoinHandle<R, ()>
+where
+    F: Future<Output = R> + Send + 'static,
+    R: Send + 'static,
+{
+    let (task, handle) = async_task::spawn(future, |task| ON_MAIN_THREAD.lock().unwrap()(task), ());
+    task.schedule();
+    handle
+}
+
+pub fn spawn_into_main_thread_with_low_priority<F, R>(future: F) -> JoinHandle<R, ()>
+where
+    F: Future<Output = R> + Send + 'static,
+    R: Send + 'static,
+{
+    let (task, handle) =
+        async_task::spawn(future, |task| ON_MAIN_THREAD_LOW_PRI.lock().unwrap()(task), ());
+    task.schedule();
+    handle
+}
+
+fn read_from_tab_pty(mut reader: Box<dyn std::io::Read>) {
     const BUFSIZE: usize = 32 * 1024;
     let mut buf = [0; BUFSIZE];
 
     let mut lim =
-        RateLimiter::new(config.ratelimit_output_bytes_per_second.unwrap_or(2 * 1024 * 1024));
+        RateLimiter::new(2 * 1024 * 1024);
 
     loop {
         match reader.read(&mut buf) {
@@ -39,73 +113,40 @@ fn read_from_tab_pty(config: Arc<Config>, mut reader: Box<dyn std::io::Read>) {
             Ok(size) => {
                 lim.blocking_admittance_check(size as u32);
                 let data = buf[0..size].to_vec();
-                promise::spawn_into_main_thread_with_low_priority(async move {
+                spawn_into_main_thread_with_low_priority(async move {
                     let mux = Mux::get().unwrap();
                     let tab = mux.get_tab();
-                    tab.advance_bytes(&data, &mut Host { writer: &mut *tab.writer() });
+                    // tab.advance_bytes(&data, &mut Host { writer: &mut *tab.writer() });
                 });
             }
         }
     }
 }
 
-struct Host<'a> {
-    writer: &'a mut dyn std::io::Write,
-}
-
-impl<'a> TerminalHost for Host<'a> {
-    fn writer(&mut self) -> &mut dyn std::io::Write {
-        &mut self.writer
-    }
-
-    fn click_link(&mut self, link: &Arc<Hyperlink>) {
-        match open::that(link.uri()) {
-            Ok(_) => {}
-            Err(_) => {}
-        }
-    }
-
-    fn get_clipboard(&mut self) -> anyhow::Result<Arc<dyn Clipboard>> {
-        bail!("peer requested clipboard; ignoring");
-    }
-
-    fn set_title(&mut self, _title: &str) {}
-}
+// struct Host<'a> {
+//     writer: &'a mut dyn std::io::Write,
+// }
 
 thread_local! {
     static MUX: RefCell<Option<Rc<Mux>>> = RefCell::new(None);
 }
 
 impl Mux {
-    pub fn new(config: &Arc<Config>, size: PtySize) -> anyhow::Result<Self> {
+    pub fn new(size: PtySize) -> anyhow::Result<Self> {
         let pty_system = Box::new(unix::UnixPtySystem);
         let pair = pty_system.openpty(size)?;
         let child = pair.slave.spawn_command(Command::new(crate::pty::get_shell()?))?;
 
-        let terminal = crate::term::Terminal::new(
-            size.rows as usize,
-            size.cols as usize,
-            size.pixel_width as usize,
-            size.pixel_height as usize,
-            config.scrollback_lines.unwrap_or(3500),
-            config.hyperlink_rules.clone(),
-        );
+        let tab = Tab::new(child, pair.master);
 
-        let tab = Tab::new(terminal, child, pair.master);
-
-        Ok(Self { tab: RefCell::new(tab), config: Arc::clone(config) })
+        Ok(Self { tab: RefCell::new(tab) })
     }
 
     pub fn start(&self) -> anyhow::Result<()> {
         let reader = self.tab.borrow().reader()?;
-        let config = Arc::clone(&self.config);
-        thread::spawn(move || read_from_tab_pty(config, reader));
+        thread::spawn(move || read_from_tab_pty(reader));
 
         Ok(())
-    }
-
-    pub fn config(&self) -> &Arc<Config> {
-        &self.config
     }
 
     pub fn set_mux(mux: &Rc<Mux>) {
--- src/mux/tab.rs
+++ src/mux/tab.rs
@@ -1,106 +1,19 @@
-use crate::core::promise;
-use crate::mux::Mux;
-use crate::pty::{Child, MasterPty, PtySize};
-use crate::term::color::ColorPalette;
-use crate::term::{KeyCode, KeyModifiers, MouseEvent, Terminal, TerminalHost};
+use crate::pty::{Child, MasterPty};
 use std::cell::{RefCell, RefMut};
-use std::sync::{Arc, Mutex};
-
-const PASTE_CHUNK_SIZE: usize = 1024;
-
-struct Paste {
-    text: String,
-    offset: usize,
-}
-
-fn schedule_next_paste(paste: &Arc<Mutex<Paste>>) {
-    let paste = Arc::clone(paste);
-    promise::spawn(async move {
-        let mut locked = paste.lock().unwrap();
-        let mux = Mux::get().unwrap();
-        let tab = mux.get_tab();
-
-        let remain = locked.text.len() - locked.offset;
-        let chunk = remain.min(PASTE_CHUNK_SIZE);
-        let text_slice = &locked.text[locked.offset..locked.offset + chunk];
-        tab.send_paste(text_slice).unwrap();
-
-        if chunk < remain {
-            locked.offset += chunk;
-            schedule_next_paste(&paste);
-        }
-    });
-}
 
 pub struct Tab {
-    terminal: RefCell<Terminal>,
     process: RefCell<Box<dyn Child>>,
     pty: RefCell<Box<dyn MasterPty>>,
     can_close: bool,
 }
 
 impl Tab {
-    pub fn renderer(&self) -> RefMut<Terminal> {
-        RefMut::map(self.terminal.borrow_mut(), |t| &mut *t)
-    }
-
-    pub fn trickle_paste(&self, text: String) -> anyhow::Result<()> {
-        if text.len() <= PASTE_CHUNK_SIZE {
-            self.send_paste(&text)?;
-        } else {
-            self.send_paste(&text[0..PASTE_CHUNK_SIZE])?;
-
-            let paste = Arc::new(Mutex::new(Paste { text, offset: PASTE_CHUNK_SIZE }));
-            schedule_next_paste(&paste);
-        }
-        Ok(())
-    }
-
-    pub fn advance_bytes(&self, buf: &[u8], host: &mut dyn TerminalHost) {
-        self.terminal.borrow_mut().advance_bytes(buf, host)
-    }
-
-    pub fn mouse_event(
-        &self,
-        event: MouseEvent,
-        host: &mut dyn TerminalHost,
-    ) -> anyhow::Result<()> {
-        self.terminal.borrow_mut().mouse_event(event, host)
-    }
-
-    pub fn key_down(&self, key: KeyCode, mods: KeyModifiers) -> anyhow::Result<()> {
-        self.terminal.borrow_mut().key_down(key, mods, &mut *self.pty.borrow_mut())
-    }
-
-    pub fn resize(&self, size: PtySize) -> anyhow::Result<()> {
-        self.pty.borrow_mut().resize(size)?;
-        self.terminal.borrow_mut().resize(
-            size.rows as usize,
-            size.cols as usize,
-            size.pixel_width as usize,
-            size.pixel_height as usize,
-        );
-        Ok(())
-    }
-
     pub fn writer(&self) -> RefMut<dyn std::io::Write> {
         self.pty.borrow_mut()
     }
 
     pub fn reader(&self) -> anyhow::Result<Box<dyn std::io::Read + Send>> {
         self.pty.borrow_mut().try_clone_reader()
-    }
-
-    fn send_paste(&self, text: &str) -> anyhow::Result<()> {
-        self.terminal.borrow_mut().send_paste(text, &mut *self.pty.borrow_mut())
-    }
-
-    pub fn get_title(&self) -> String {
-        self.terminal.borrow_mut().get_title().to_string()
-    }
-
-    pub fn palette(&self) -> ColorPalette {
-        self.terminal.borrow().palette().clone()
     }
 
     pub fn close(&mut self) {
@@ -119,9 +32,8 @@ impl Tab {
         }
     }
 
-    pub fn new(terminal: Terminal, process: Box<dyn Child>, pty: Box<dyn MasterPty>) -> Self {
+    pub fn new(process: Box<dyn Child>, pty: Box<dyn MasterPty>) -> Self {
         Self {
-            terminal: RefCell::new(terminal),
             process: RefCell::new(process),
             pty: RefCell::new(pty),
             can_close: false,
